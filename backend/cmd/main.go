8203450ebf482a654ab653b868f4ac72ea390668
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-04-07 10:14:36 +0700

    feat: init project backend and front end

---
+++ backend/cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func main() {
+	godotenv.Load()
+	r := gin.Default()
+	fmt.Println("tets")
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.Run(fmt.Sprintf(":%s", os.Getenv("PORT")))
+
+}
