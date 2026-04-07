f7f5abcbf173611799023074fdf83955d0a1bd76
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-04-07 14:41:18 +0700

    feat: add protected routes in create link

--- backend/internal/routes/links_routes.go
+++ backend/internal/routes/links_routes.go
@@ -2,10 +2,13 @@ package routes
 
 import (
 	"linksort/internal/handler"
+	"linksort/internal/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func LinksRoutes(r *gin.Engine, h *handler.LinksHandler) {
-	r.POST("/api/links", h.CreateLink)
+	admin := r.Group("/api")
+	admin.Use(middleware.JWTMiddleware())
+	admin.POST("/links", h.CreateLink)
 }
