56d85c99ec848f7d4b221296eede29ddd92d706a
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-04-07 21:17:29 +0700

    feat: add login prefix page in

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '#/index.css'
 import App from '#/App.jsx'
+import Login from '#/pages/Login.jsx'
 import { createBrowserRouter, RouterProvider, } from "react-router";
 
 let router = createBrowserRouter([
   {
     path: "/",
     Component: App,
+  },
+  {
+    path: "/Login",
+    Component: Login,
   },
 ]);
 
