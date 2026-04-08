d2f113770e120f85c040c4a31b86375dc593fc68
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-04-08 11:04:17 +0700

    chore: re init godoc/swagger

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -7,12 +7,21 @@ import (
 	"linksort/internal/routes"
 	"os"
 
+	_ "linksort/docs"
+
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title Short Link API
+// @version 1.0
+// @description API for managing short links
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+// @description Type "Bearer" followed by a space and your JWT token.
 func main() {
 	godotenv.Load()
 	r := gin.Default()
